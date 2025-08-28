c27f341e64a327b0bda7c2ea76099c94491ffa3f
Gideon Sebastian Geier <gideon.sebastian.geier@huawei.com>
2025-08-28 16:37:19 +0800

    mmap64 interceptor
    Signed-off-by: Gideon Sebastian Geier <gideon.sebastian.geier@huawei.com>

--- src/mod/mman.c
+++ src/mod/mman.c
@@ -28,6 +28,27 @@ INTERPOSE(void *, mmap, void *addr, size_t length, int prot, int flags, int fd,
     return ev.ret;
 }
 
+INTERPOSE(void *, mmap64, void *addr, size_t length, int prot, int flags, int fd,
+          off_t offset)
+{
+    struct mmap_event ev = {
+        .pc     = INTERPOSE_PC,
+        .addr   = addr,
+        .length = length,
+        .prot   = prot,
+        .flags  = flags,
+        .fd     = fd,
+        .offset = offset,
+        .ret    = 0,
+    };
+
+    metadata_t md = {0};
+    PS_PUBLISH(INTERCEPT_BEFORE, EVENT_MMAP, &ev, &md);
+    ev.ret = REAL(mmap64, addr, length, prot, flags, fd, offset);
+    PS_PUBLISH(INTERCEPT_AFTER, EVENT_MMAP, &ev, &md);
+    return ev.ret;
+}
+
 INTERPOSE(int, munmap, void *addr, size_t length)
 {
     struct munmap_event ev = {
