d6316de921b79f4d31d0984042f85b00b3bf3dfb
Diogo Behrens <diogo.behrens@huawei.com>
2025-09-19 11:46:13 +0000

    test/trace-checker: Add EXPECTED_SOME to make traces more flexible
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

--- test/checkers/trace_checker.h
+++ test/checkers/trace_checker.h
@@ -17,6 +17,8 @@
 struct expected_event {
     chain_id chain;
     type_id type;
+    unsigned atleast;
+    unsigned atmost;
     bool wild;
     bool set;
 };
@@ -24,19 +26,28 @@ struct expected_event {
 #define EXPECTED_EVENT(CHAIN, TYPE)                                            \
     (struct expected_event)                                                    \
     {                                                                          \
-        .chain = CHAIN, .type = TYPE, .wild = false, .set = true,              \
+        .chain = CHAIN, .type = TYPE, .atleast = 1, .atmost = 1,               \
+        .wild = false, .set = true,                                            \
     }
 #define EXPECTED_SUFFIX(CHAIN, TYPE)                                           \
     (struct expected_event)                                                    \
     {                                                                          \
-        .chain = CHAIN, .type = TYPE, .wild = true, .set = true,               \
+        .chain = CHAIN, .type = TYPE, .atleast = 1, .atmost = 1, .wild = true, \
+        .set = true,                                                           \
+    }
+#define EXPECTED_SOME(CHAIN, TYPE, ATLEAST, ATMOST)                            \
+    (struct expected_event)                                                    \
+    {                                                                          \
+        .chain = CHAIN, .type = TYPE, .atleast = ATLEAST, .atmost = ATMOST,    \
+        .wild = false, .set = true,                                            \
     }
 #define EXPECTED_ANY_SUFFIX EXPECTED_SUFFIX(0, 0)
 
 #define EXPECTED_END                                                           \
     (struct expected_event)                                                    \
     {                                                                          \
-        .chain = 0, .type = 0, .wild = false, .set = false,                    \
+        .chain = 0, .type = 0, .atleast = 0, .atmost = 0, .wild = false,       \
+        .set = false,                                                          \
     }
 
 
