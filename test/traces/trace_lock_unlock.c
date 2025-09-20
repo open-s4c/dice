51c3675753403e52cfa2d6fa5f9e2dcfcde30c18
Diogo Behrens <db7@sdf.org>
2025-09-20 14:35:11 +0200

    cmake: Add option to measure test coverage
    Signed-off-by: Diogo Behrens <db7@sdf.org>

--- test/traces/trace_lock_unlock.c
+++ test/traces/trace_lock_unlock.c
@@ -5,6 +5,7 @@
 #include <trace_checker.h>
 
 #include <dice/chains/capture.h>
+#include <dice/events/memaccess.h>
 #include <dice/events/pthread.h>
 #include <dice/events/self.h>
 #include <dice/events/thread.h>
@@ -13,8 +14,15 @@
 
 struct expected_event expected[] = {
     EXPECTED_EVENT(CAPTURE_EVENT, EVENT_SELF_INIT),
+    EXPECTED_SUFFIX(CAPTURE_EVENT, EVENT_THREAD_START),
     EXPECTED_SUFFIX(CAPTURE_BEFORE, EVENT_PTHREAD_MUTEX_LOCK),
     EXPECTED_EVENT(CAPTURE_AFTER, EVENT_PTHREAD_MUTEX_LOCK),
+
+    // when checking coverage, these are also triggered
+    EXPECTED_SOME(CAPTURE_EVENT, EVENT_MA_READ, 0, 1),
+    EXPECTED_SOME(CAPTURE_EVENT, EVENT_MA_WRITE, 0, 1),
+    // end
+
     EXPECTED_EVENT(CAPTURE_BEFORE, EVENT_PTHREAD_MUTEX_UNLOCK),
     EXPECTED_EVENT(CAPTURE_AFTER, EVENT_PTHREAD_MUTEX_UNLOCK),
     EXPECTED_SUFFIX(CAPTURE_EVENT, EVENT_SELF_FINI),
