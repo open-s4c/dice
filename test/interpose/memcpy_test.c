f1e620804809521ea688f160edb3a876a2f8e858
gigna <georgeta.igna@huawei.com>
2025-10-23 12:21:36 +0200

    Removed the mock/fake functions from memcpy_test as Clang overrides the intended return values of the mocks.
        Signed-off-by: gigna <georgeta.igna@huawei.com>

--- test/interpose/memcpy_test.c
+++ test/interpose/memcpy_test.c
@@ -4,9 +4,6 @@
  */
 #include <stddef.h>
 #include <stdlib.h>
-#define memcpy fake_memcpy
-#define memmove fake_memmove
-#define memset fake_memset
 #include <string.h>
 
 #define DICE_TEST_INTERPOSE
@@ -15,34 +12,6 @@
 #include <dice/interpose.h>
 #include <dice/pubsub.h>
 #include <dice/events/memcpy.h>
-
-static void *symbol;
-/* we need to declare this as noinline, otherwise the optimization of the
- * compiler gets rid of the symbol. */
-static __attribute__((noinline)) void
-enable(void *foo)
-{
-    symbol = foo;
-}
-static __attribute__((noinline)) void
-disable(void)
-{
-    symbol = NULL;
-}
-static inline bool
-enabled(void)
-{
-    return symbol != NULL;
-}
-
-void *
-real_sym(const char *name, const char *ver)
-{
-    (void)ver;
-    if (!enabled())
-        return _real_sym(name, ver);
-    return symbol;
-}
 
 /* Expects struct to match this:
  *
@@ -75,44 +44,10 @@ struct memmove_event E_memmove;
  */
 struct memset_event E_memset;
 
-/* mock implementation of functions */
-void *
-fake_memcpy(void *dest ,const void *src ,size_t num)
-{
-    /* check that every argument is as expected */
-    ensure(dest == E_memcpy.dest);
-    ensure(src == E_memcpy.src);
-    ensure(num == E_memcpy.num);
-    /* return expected value */
- return E_memcpy.ret;
-}
-void *
-fake_memmove(void *dest ,const void *src ,size_t count)
-{
-    /* check that every argument is as expected */
-    ensure(dest == E_memmove.dest);
-    ensure(src == E_memmove.src);
-    ensure(count == E_memmove.count);
-    /* return expected value */
- return E_memmove.ret;
-}
-void *
-fake_memset(void *ptr, int value, size_t num)
-{
-    /* check that every argument is as expected */
-    ensure(ptr == E_memset.ptr);
-    ensure(value == E_memset.value);
-    ensure(num == E_memset.num);
-    /* return expected value */
- return E_memset.ret;
-}
-
 #define ASSERT_FIELD_EQ(E, field)                                              \
     ensure(memcmp(&ev->field, &(E)->field, sizeof(__typeof((E)->field))) == 0);
 
 PS_SUBSCRIBE(INTERCEPT_BEFORE, EVENT_MEMCPY, {
-    if (!enabled())
-        return PS_STOP_CHAIN;
     struct memcpy_event *ev = EVENT_PAYLOAD(ev);
     ASSERT_FIELD_EQ(&E_memcpy, dest);
     ASSERT_FIELD_EQ(&E_memcpy, src);
@@ -120,17 +55,14 @@ PS_SUBSCRIBE(INTERCEPT_BEFORE, EVENT_MEMCPY, {
 })
 
 PS_SUBSCRIBE(INTERCEPT_AFTER, EVENT_MEMCPY, {
-    if (!enabled())
-        return PS_STOP_CHAIN;
     struct memcpy_event *ev = EVENT_PAYLOAD(ev);
     ASSERT_FIELD_EQ(&E_memcpy, dest);
     ASSERT_FIELD_EQ(&E_memcpy, src);
     ASSERT_FIELD_EQ(&E_memcpy, num);
  ASSERT_FIELD_EQ(&E_memcpy, ret);
+ ensure(E_memcpy.dest == E_memcpy.ret);
 })
 PS_SUBSCRIBE(INTERCEPT_BEFORE, EVENT_MEMMOVE, {
-    if (!enabled())
-        return PS_STOP_CHAIN;
     struct memmove_event *ev = EVENT_PAYLOAD(ev);
     ASSERT_FIELD_EQ(&E_memmove, dest);
     ASSERT_FIELD_EQ(&E_memmove, src);
@@ -138,8 +70,6 @@ PS_SUBSCRIBE(INTERCEPT_BEFORE, EVENT_MEMMOVE, {
 })
 
 PS_SUBSCRIBE(INTERCEPT_AFTER, EVENT_MEMMOVE, {
-    if (!enabled())
-        return PS_STOP_CHAIN;
     struct memmove_event *ev = EVENT_PAYLOAD(ev);
     ASSERT_FIELD_EQ(&E_memmove, dest);
     ASSERT_FIELD_EQ(&E_memmove, src);
@@ -147,8 +77,6 @@ PS_SUBSCRIBE(INTERCEPT_AFTER, EVENT_MEMMOVE, {
  ASSERT_FIELD_EQ(&E_memmove, ret);
 })
 PS_SUBSCRIBE(INTERCEPT_BEFORE, EVENT_MEMSET, {
-    if (!enabled())
-        return PS_STOP_CHAIN;
     struct memset_event *ev = EVENT_PAYLOAD(ev);
     ASSERT_FIELD_EQ(&E_memset, ptr);
     ASSERT_FIELD_EQ(&E_memset, value);
@@ -156,8 +84,6 @@ PS_SUBSCRIBE(INTERCEPT_BEFORE, EVENT_MEMSET, {
 })
 
 PS_SUBSCRIBE(INTERCEPT_AFTER, EVENT_MEMSET, {
-    if (!enabled())
-        return PS_STOP_CHAIN;
     struct memset_event *ev = EVENT_PAYLOAD(ev);
     ASSERT_FIELD_EQ(&E_memset, ptr);
     ASSERT_FIELD_EQ(&E_memset, value);
@@ -165,61 +91,57 @@ PS_SUBSCRIBE(INTERCEPT_AFTER, EVENT_MEMSET, {
  ASSERT_FIELD_EQ(&E_memset, ret);
 })
 
-
-static void
-event_init(void *ptr, size_t n)
-{
-    char *buf = ptr;
-    for (size_t i = 0; i < n; i++)
-        buf[i] = rand() % 256;
-}
-
-/* test case */
+/* test cases */
 
 static void
 test_memcpy(void)
 {
-    /* initialize event with random content */
-    event_init(&E_memcpy, sizeof(struct memcpy_event));
-    /* call memcpy with arguments */
-    enable(fake_memcpy);
+    char dest[10];
+    E_memcpy.dest = dest;
+    char hello[] = "Hello!";
+    E_memcpy.src= hello;
+    E_memcpy.num = strlen(E_memcpy.src) + 1;
+    E_memcpy.ret = E_memcpy.dest;
      void *  ret =                                   //
                                  memcpy(                                    //
                                      E_memcpy.dest,                           //
                                      E_memcpy.src,                           //
                                      E_memcpy.num                                  );
- ensure(ret == E_memcpy.ret);
-    //disable();
+    ensure(ret == E_memcpy.dest);
+    ensure(strcmp(E_memcpy.dest, E_memcpy.src) == 0);
 }
 static void
 test_memmove(void)
 {
-    /* initialize event with random content */
-    event_init(&E_memmove, sizeof(struct memmove_event));
-    /* call memmove with arguments */
-    enable(fake_memmove);
+    char dest[10];
+    E_memmove.dest = dest;
+    char hello[] = "Hi there!";
+    E_memmove.src= hello;
+    E_memmove.count = 2;
+    E_memmove.ret = E_memmove.dest;
      void *  ret =                                   //
                                  memmove(                                    //
                                      E_memmove.dest,                           //
                                      E_memmove.src,                           //
                                      E_memmove.count                                  );
- ensure(ret == E_memmove.ret);
-    disable();
+    ensure(ret == E_memmove.dest);
+    log_printf("memmove res %s\n", (char *)E_memmove.dest);
+    ensure(strncmp((char *)E_memmove.dest, "Hi", 2) == 0);
 }
 static void
 test_memset(void)
 {
-    /* initialize event with random content */
-    event_init(&E_memset, sizeof(struct memset_event));
-    /* call memset with arguments */
-    enable(fake_memset);
+    char dest[5];
+    E_memset.ptr = dest;
+    E_memset.value= 3;
+    E_memset.num = sizeof(dest);
+    E_memset.ret = E_memset.ptr;
      void *  ret =                                   //
                                  memset(                                    //
                                      E_memset.ptr,                           //
                                      E_memset.value,                           //
                                      E_memset.num                                  );
- ensure(ret == E_memset.ret);
-    disable();
+    ensure(ret == E_memset.ptr);
 }
 
 int
