7b8fbaca7bb6efcbb1c0415a641763dcf8dabedf
Diogo Behrens <diogo.behrens@huawei.com>
2025-10-24 09:30:03 +0000

    box: Add self-box to hide self's symbols
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

--- src/mod/self.c
+++ src/mod/self.c
@@ -106,22 +106,22 @@ tls_fini_(struct self *self)
 // public interface
 // -----------------------------------------------------------------------------
 
-DICE_HIDE_IF thread_id
-self_id(metadata_t *md)
+DICE_HIDE thread_id
+self_id_(metadata_t *md)
 {
     struct self *self = (struct self *)md;
     return self ? self->tid : NO_THREAD;
 }
 
-DICE_HIDE_IF bool
-self_retired(metadata_t *md)
+DICE_HIDE bool
+self_retired_(metadata_t *md)
 {
     struct self *self = (struct self *)md;
     return self ? self->retired : false;
 }
 
-DICE_HIDE_IF void *
-self_tls_get(metadata_t *md, uintptr_t item_key)
+DICE_HIDE void *
+self_tls_get_(metadata_t *md, uintptr_t item_key)
 {
     struct self *self = (struct self *)md;
 
@@ -138,9 +138,9 @@ self_tls_get(metadata_t *md, uintptr_t item_key)
     return i->ptr;
 }
 
-DICE_HIDE_IF void
-self_tls_set(metadata_t *md, uintptr_t item_key, void *ptr,
-             struct tls_dtor dtor)
+DICE_HIDE void
+self_tls_set_(metadata_t *md, uintptr_t item_key, void *ptr,
+              struct tls_dtor dtor)
 {
     struct self *self = (struct self *)md;
 
@@ -189,8 +189,8 @@ mempool_free_dtor_(void *arg, void *ptr)
     mempool_free(ptr);
 }
 
-DICE_HIDE_IF void *
-self_tls(metadata_t *md, const void *global, size_t size)
+DICE_HIDE void *
+self_tls_(metadata_t *md, const void *global, size_t size)
 {
     uintptr_t item_key = (uintptr_t)global;
 
@@ -210,6 +210,37 @@ self_tls(metadata_t *md, const void *global, size_t size)
                  });
 
     return ptr;
+}
+
+DICE_WEAK thread_id
+self_id(metadata_t *md)
+{
+    return self_id_(md);
+}
+
+DICE_WEAK bool
+self_retired(metadata_t *md)
+{
+    return self_retired_(md);
+}
+
+DICE_WEAK void *
+self_tls_get(metadata_t *md, uintptr_t item_key)
+{
+    return self_tls_get_(md, item_key);
+}
+
+DICE_WEAK void
+self_tls_set(metadata_t *md, uintptr_t item_key, void *ptr,
+             struct tls_dtor dtor)
+{
+    self_tls_set_(md, item_key, ptr, dtor);
+}
+
+DICE_WEAK void *
+self_tls(metadata_t *md, const void *global, size_t size)
+{
+    return self_tls_(md, global, size);
 }
 
 // -----------------------------------------------------------------------------
