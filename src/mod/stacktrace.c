f2a8796d9d32c074e7fdb1b5e57d4905937b537d
Diogo Behrens <diogo.behrens@huawei.com>
2025-11-12 10:44:49 +0000

    perf: Further inlining optimizations
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

--- src/mod/stacktrace.c
+++ src/mod/stacktrace.c
@@ -23,8 +23,7 @@ PS_ADVERTISE_TYPE(EVENT_STACKTRACE_EXIT)
 /* Mark module initialization (optional) */
 DICE_MODULE_INIT()
 
-
-static void check_main_start_(void *caller);
+static inline void check_main_start_(void *caller);
 
 void
 __tsan_func_entry(void *caller)
