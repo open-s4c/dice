70958e18c7f238ada988cf1556f4b59224560ee7
Diogo Behrens <diogo.behrens@huawei.com>
2025-12-09 13:39:45 +0000

    cicd: Reenable smolBSD pipeline
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

--- .github/smol/run.sh
+++ .github/smol/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This script runs in the cicd-ARCH.img called by /etc/rc.
 # It assumes the payload image (containing this file too) is mounted at /mnt
-# It will first untar the example build it and then run it
+# It will first untar the project and then run it
 set -e
 
 fail() {
@@ -9,13 +9,16 @@ fail() {
 }
 
 echo "extract code..."
-tar -xf /mnt/example.tar -C / || fail
+mkdir -p /project
+tar -xf /mnt/project.tar -C /project || fail
 (
-	cd /example
+	cd /project
+	rm -f /mnt/tests.ok
+	ls -R .
 	echo "configure and build ..."
 	cmake -S. -Bbuild || fail
 	cmake --build build || fail
 	echo "test..."
-	ctest --test-dir build --output-on-failure 2>&1 | tee /mnt/tests.log || fail
+	ctest --test-dir build --output-on-failure > /mnt/tests.log 2>&1 || fail
 	touch /mnt/tests.ok
 )
