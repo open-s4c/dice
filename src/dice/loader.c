cff745c9af791d84e7aa41b2a63d372e50f46830
Diogo Behrens <diogo.behrens@huawei.com>
2026-01-14 12:45:11 +0000

    feat: add compile definition to disable loader
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

--- src/dice/loader.c
+++ src/dice/loader.c
@@ -30,6 +30,7 @@ init_()
     ps_init_();
 }
 
+#ifndef DICE_DISABLE_LOADER
 static void
 load_plugin_(const char *path)
 {
@@ -75,3 +76,4 @@ PS_SUBSCRIBE(CHAIN_CONTROL, EVENT_DICE_INIT, {
     }
     log_debug("[%4d] DONE: %s", DICE_MODULE_SLOT, __FILE__);
 })
+#endif
